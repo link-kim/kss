1f276cab565588528f41fe6aba248ba8ffbddc23
link-kim <link@rtzr.ai>
2022-03-20 22:38:06 +0000

    Version-up to 3.6.1(rebase upstream kss version update v3.4->v3.4.2)

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="rt-kss",
-    version="3.6.0",
+    version="3.6.1",
     author="Hyunwoong Ko",
     author_email="kevin.ko@tunib.ai",
     url="https://github.com/hyunwoongko/kss",
